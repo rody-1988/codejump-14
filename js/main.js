a6e45905e7d5ee8b075858e72bd87a4816f79643
rody-1988 <xl.pogigori.808@gmail.com>
2026-03-23 13:56:00 +0900

    ハンバーガー調整中2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
-const hamabuer = document.querySelector(".header__hamburger");
+const hamburger = document.querySelector(".header__hamburger");
 const nav = document.querySelector(".header-nav");
 const mask = document.querySelector(".header-mask");
 
-hamabuer.addEventListener("click", function () {
-  hamabuer.classList.toggle("is-active");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("is-active");
   nav.classList.toggle("is-active");
   mask.classList.toggle("is-active");
 });
 mask.addEventListener("click", function () {
-  hamabuer.classList.remove("is-active");
+  hamburger.classList.remove("is-active");
   nav.classList.remove("is-active");
   mask.classList.remove("is-active");
+});
+
+const navLinks = document.querySelectorAll(".header-nav a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+
+    hamburger.classList.remove("is-active");
+    nav.classList.remove("is-active");
+    mask.classList.remove("is-active");
+  });
 });
