5c8e1131a9c7fa6a2fabea59b8825afff784e3c2
rody-1988 <xl.pogigori.808@gmail.com>
2026-03-24 13:27:25 +0900

    仮完成

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// ハンバーガーメニュー
+
 const hamburger = document.querySelector(".header__hamburger");
 const nav = document.querySelector(".header-nav");
 const mask = document.querySelector(".header-mask");
@@ -7,12 +9,15 @@ hamburger.addEventListener("click", function () {
   nav.classList.toggle("is-active");
   mask.classList.toggle("is-active");
 });
+
+// どこを押しても消える
 mask.addEventListener("click", function () {
   hamburger.classList.remove("is-active");
   nav.classList.remove("is-active");
   mask.classList.remove("is-active");
 });
 
+//メニューのリンク押してもきえる
 const navLinks = document.querySelectorAll(".header-nav a");
 
 navLinks.forEach(link => {
@@ -21,5 +26,30 @@ navLinks.forEach(link => {
     hamburger.classList.remove("is-active");
     nav.classList.remove("is-active");
     mask.classList.remove("is-active");
+  });
+});
+
+// トップに戻る
+
+// id　#to-Topを探す
+const toTop = document.querySelector('#to-top');
+
+// スクロールの監視
+window.addEventListener('scroll', ()=> {
+  // 700px 以上スクロールしたら、クラスをつける
+  if (window.scrollY > 700) {
+    toTop.classList.add('is-show');
+  } else {
+    // 700px 未満ならクラスを外す
+    toTop.classList.remove('is-show')
+  }
+});
+
+toTop.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
   });
 });
